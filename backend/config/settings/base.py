c4859575d955e8f7c0cca793067a695242028fc4
eastcoast <princeadewale391@gmail.com>
2026-07-16 10:26:00 +0100

    used a working settings to setup settings

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
