1a1d9c73e9af0182e96ebbbaccc5132d5bd1d6ef
eastcoast <princeadewale391@gmail.com>
2026-07-24 09:32:24 +0100

    tried

--- web/entercom/src/shared/components/AppPreloader.tsx
+++ web/entercom/src/shared/components/AppPreloader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import logo from '../assets/logo.png';
 
 export function AppPreloader({ message = 'Initializing...' }: { message?: string }) {
